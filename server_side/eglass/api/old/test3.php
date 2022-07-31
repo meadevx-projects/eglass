9471e6b32e9e03ca6531c63365bc077946447b8b
Maneesh Abraham <mea.devx@gmail.com>
2022-07-30 21:25:25 -0700

    remove old files

--- server_side/eglass/api/old/test3.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set("display_errors",1);
-require_once '../lib/tesseract_ocr/TesseractOCR/TesseractOCR.php';
-$tesseract = new TesseractOCR('myimage.png');
-$text = $tesseract->recognize();
-echo PHP_EOL, "The recognized text is:", $text, PHP_EOL;
-
-?>
-
